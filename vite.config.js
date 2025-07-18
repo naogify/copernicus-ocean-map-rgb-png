62652fa3df0ffe5d485d3d026fe5a75984b64e49
naoki <n.globe.us@gmail.com>
2025-07-18 18:15:44 +0900

    some fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/jma-wind-map/' : '/',
+  base: process.env.GITHUB_PAGES ? '/copernicus-ocean-map-rgb-png/' : '/',
   build: {
     sourcemap: true
   }
